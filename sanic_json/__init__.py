f59627a37ed87ed0ebac4faaed8fa721bc5a2d99
weaming <garden.yuen@gmail.com>
2018-08-24 17:31:34 +0800

    extract check_result()

--- sanic_json/__init__.py
+++ sanic_json/__init__.py
@@ -2,35 +2,46 @@ from functools import partial
 from sanic.response import json
 
 
-def response_check(fn):
+def check_result(rv):
+    if isinstance(rv, tuple):
+        rv = rv[0]
+        other = rv[1]
+        if isinstance(other, dict):
+            rv_kw = other
+        elif isinstance(other, int):
+            rv_kw = {"status": other}
+        else:
+            raise Exception("unaccepted second return value: {}".format(other))
+    else:
+        rv_kw = {}
+
+    if isinstance(rv, dict):
+        if 'success' not in rv:
+            rv['success'] = True if rv_kw.get('status', 200) == 200 else False
+
+        return json(rv, **rv_kw)
+    else:
+        return rv
+
+
+def check_response(fn):
     async def new_fn(*args, **kwargs):
         try:
             rv = await fn(*args, **kwargs)
         except Exception as e:
             rv = ({"success": False, "reason": str(e), "type": str(type(e))}, 500)
 
-        if isinstance(rv, tuple):
-            other = rv[1]
-            if isinstance(other, dict):
-                rv_kw = other
-            elif isinstance(other, int):
-                rv_kw = {"status": other}
-            else:
-                raise Exception("unaccepted second return value: {}".format(other))
-            rv = rv[0]
-        else:
-            rv_kw = {}
-
-        if isinstance(rv, dict):
-            return json(rv, **rv_kw)
-        else:
-            return rv
+        return check_result(rv)
 
     return new_fn
 
 
+def add_route(app, url, fn):
+    app.route(url)(fn)
+
+
 def json_route(app, url, fn):
-    app.route(url)(response_check(fn))
+    add_route(app, url, check_response(fn))
 
 
 def get_json_route(app):
